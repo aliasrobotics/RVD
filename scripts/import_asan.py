60eefcb304b47097f5183b57a006c9d093dead96
LanderU <lander.usategui@gmail.com>
2019-08-04 12:03:17 +0200

    import_asan.py:
    * Fix several code style violations
    
    Signed-off-by: LanderU <lander.usategui@gmail.com>

--- scripts/import_asan.py
+++ scripts/import_asan.py
@@ -7,6 +7,7 @@ from import_base import RVDImport
 from time import gmtime, strftime
 from sys import argv
 
+
 class RVDImport_ASan(RVDImport):
     
     def __init__(self, username="vmayoral", repo="test"):
@@ -15,7 +16,7 @@ class RVDImport_ASan(RVDImport):
         self.username = username
         self.repo_name = repo
         self.repo = self.g.get_repo(self.username+"/"+self.repo_name)
-        self.issues = [] # stores the name of each one of the issues in the corresponding repository
+        self.issues = []  # stores the name of each one of the issues in the corresponding repository
         self.csv_elements = None
         
     def list_repos(self):
@@ -48,11 +49,11 @@ class RVDImport_ASan(RVDImport):
                 #   package,error_name,stack_trace_key,count,sample_stack_trace
                 # let's make a dict out of it
                 elem = {
-                    'package':row['package'],
-                    'error_name':row['error_name'],
-                    'stack_trace_key':row['stack_trace_key'],
-                    'count':row['count'],
-                    'sample_stack_trace':row['sample_stack_trace'],
+                    'package': row['package'],
+                    'error_name': row['error_name'],
+                    'stack_trace_key': row['stack_trace_key'],
+                    'count': row['count'],
+                    'sample_stack_trace': row['sample_stack_trace'],
                 }
                 parsed_list.append(elem)
         self.csv_elements = parsed_list        
@@ -65,9 +66,7 @@ class RVDImport_ASan(RVDImport):
         """
         self.repo.create_issue(title=title, body=body, labels=labels)
         
-    def make_issue(self, dict_elem, 
-                        robot_component="ROS 2",
-                        reporter = "vmayoral"):
+    def make_issue(self, dict_elem, robot_component="ROS 2", reporter="vmayoral"):
         """
         Make a new issue
                 
@@ -90,7 +89,8 @@ class RVDImport_ASan(RVDImport):
         print("\tMaking issue with title '"+title+"'")
         self.repo.create_issue(title=title, body=body, labels=labels)        
 
-    def make_issue_title_asan(self, dict_elem):
+    @staticmethod
+    def make_issue_title_asan(dict_elem):
         """
         Make and return the title of an ASan-related weakness using markdown
         :param dict_elem: dictionary element corresponding to one of the 
@@ -104,51 +104,53 @@ class RVDImport_ASan(RVDImport):
         #       rcl: detected memory leaks, __default_zero_allocate
         title = ""
         title += str(dict_elem["package"])
-        title +=": "
+        title += ": "
         title += str(dict_elem["error_name"])
-        title +=", "
+        title += ", "
         title += str(dict_elem["stack_trace_key"]).split("/")[0]
         if len(title) > 60:
             return title[:60]+"..."
         else:
             return title
 
-
     def make_issue_body_asan(self, dict_elem, robot_component="ROS 2", reporter = "vmayoral"):
         """
         Make and return the body of an ASan-related weakness using markdown
-        :param dict_elem: dictionary element corresponding to one of the 
+        :param robot_component:
+        :param reporter:
+        :param dict_elem: dictionary element corresponding to one of the
             entries of the csv
         :return string
         """
         # Each dict_elem should have the following keys: 
         #       package,error_name,stack_trace_key,count,sample_stack_trace
         # Let's compose the body of the issue based on that assumption
-        body=""
-        body+="| Input      | Value  |"+"\n"
-        body+="|---------|--------|"+"\n"
-        body+="| Robot component | "+robot_component+" |"+"\n"
-        body+="| Vendor  | N/A |"+"\n"
-        body+="| CVE ID  | N/A  |"+"\n"
-        body+="| CWE ID  | "+ self.find_cwe(dict_elem) +" |"+"\n"
-        body+="| RVSS Score  |  N/A |"+"\n"
-        body+="| RVSS Vector | N/A |"+"\n"
-        body+="| GitHub Account | "+reporter+" |"+"\n"
-        body+="| Date Reported  | "+str(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())) +" |"+"\n"
-        body+="| Date Updated   | "+str(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())) +" |"+"\n"
-        body+="| Exploitation vector | Internal network, robotics framework |"+"\n"
-        body+="\n"
-        body+="\n"
-        body+="### Description"+"\n"
-        body+="Issue detected while running Address Sanitizer (ASan)."+"\n"
-        body+="\n"
-        body+="### Stack trace"+"\n"
-        body+="```"+"\n"
-        body+= str(dict_elem["sample_stack_trace"])+"\n"
-        body+="```"+"\n"
+        body = ""
+        body += "| Input      | Value  |"+"\n"
+        body += "|---------|--------|"+"\n"
+        body += "| Robot component | "+robot_component+" |"+"\n"
+        body += "| Vendor  | N/A |"+"\n"
+        body += "| CVE ID  | N/A  |"+"\n"
+        body += "| CWE ID  | " + self.find_cwe(dict_elem) + " |"+"\n"
+        body += "| RVSS Score  |  N/A |"+"\n"
+        body += "| RVSS Vector | N/A |"+"\n"
+        body += "| GitHub Account | "+reporter+" |"+"\n"
+        body += "| Date Reported  | "+str(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())) + " |"+"\n"
+        body += "| Date Updated   | "+str(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())) + " |"+"\n"
+        body += "| Exploitation vector | Internal network, robotics framework |"+"\n"
+        body += "\n"
+        body += "\n"
+        body += "### Description"+"\n"
+        body += "Issue detected while running Address Sanitizer (ASan)."+"\n"
+        body += "\n"
+        body += "### Stack trace"+"\n"
+        body += "```"+"\n"
+        body += str(dict_elem["sample_stack_trace"])+"\n"
+        body += "```"+"\n"
         return body
 
-    def find_cwe(self, dict_elem):
+    @staticmethod
+    def find_cwe(dict_elem):
         """
         Find out CWE id from "error_name" in the element
         :param dict_elem: dictionary element corresponding to one of the 
@@ -163,15 +165,14 @@ class RVDImport_ASan(RVDImport):
             return "[CWE-400: Uncontrolled Resource Consumption](https://cwe.mitre.org/data/definitions/400.html)"
         else:
             return "N/A"
-        
 
-    def add_new_issues(self, file="issues.csv", robot_component="ROS 2", reporter = "vmayoral"):
+    def add_new_issues(self, file="issues.csv", robot_component="ROS 2", reporter="vmayoral"):
         """
         Retrieves information from csv, determines which ones already
         exist in the RVD and produces the corresponding new issues.        
         """
         self.parse_csv(file)
-        if self.csv_elements == None:
+        if self.csv_elements is None:
             print("ERROR: No elements parsed correctly")
             sys.exit(0)
         # Fetch all issue titles, including closed ones
