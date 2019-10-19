dfdc0d2ae21d303d2b6e4913b1567058dc6793e8
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-10-19 08:05:14 +0200

    Fix linter issues

--- scripts/relabel.py
+++ scripts/relabel.py
@@ -1,16 +1,14 @@
-"""
-Scripts ro relabel issues within the RVD repository
-Run:
-    python3 summary.py
-"""
-
 from import_base import RVDImport
 from github import Label
 from time import gmtime, strftime
 import click
 
-@ade.command('remove')
-              help='The person to greet.')
+"""
+TODO: provide description and example
+Run:
+    TODO
+"""
+@click.command('remove', help='The person to greet.')
 # @click.option('--create/--no-create', help='Create .alurityhome file automatically and relaunch configuration',
               # default=False)
 def relabel_init(package):
