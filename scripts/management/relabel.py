b5f3d0dbf0ec6447f57cce718ab58b919bb4f324
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-10-25 17:02:28 +0200

    Comment so that flake8 accepts it

--- scripts/management/relabel.py
+++ scripts/management/relabel.py
@@ -1,27 +1,27 @@
-from import_base import RVDImport
-from github import Label
-from time import gmtime, strftime
-import click
-
-"""
-WIP: Script to relabel reports
-Run:
-    TODO
-"""
-@click.command('remove', help='The person to greet.')
-# @click.option('--create/--no-create', help='Create .alurityhome file automatically and relaunch configuration',
-              # default=False)
-def relabel_init(package):
-    """
-    TODO
-    """
-    pass
-
-              
-def relabel(label1, label2):
-    """WIP: Simple script that relabels from label1 to label2."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
-
-if __name__ == '__main__':
-    relabel()
+# from import_base import RVDImport
+# from github import Label
+# from time import gmtime, strftime
+# import click
+# 
+# """
+# WIP: Script to relabel reports
+# Run:
+#     TODO
+# """
+# @click.command('remove', help='The person to greet.')
+# # @click.option('--create/--no-create', help='Create .alurityhome file automatically and relaunch configuration',
+#               # default=False)
+# def relabel_init(package):
+#     """
+#     TODO
+#     """
+#     pass
+# 
+# 
+# def relabel(label1, label2):
+#     """WIP: Simple script that relabels from label1 to label2."""
+#     for x in range(count):
+#         click.echo('Hello %s!' % name)
+# 
+# if __name__ == '__main__':
+#     relabel()
